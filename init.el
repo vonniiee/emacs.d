7ff075d09b1a4d613d40634f20ce57a2eb1396c6
rvvvr <me@visu.do>
2024-03-02 10:51:30 -0800

    multi-vterm

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
  '(custom-safe-themes
    '("c2c63381042e2e4686166d5d59a09118017b39003e58732b31737deeed454f1c" "a49795828869471ffeeb3753c61e265a75b33fed7c7b7614ee17663f1ef4b3b0" "658f6e811d9972cf53b52b00fa050ef8b30290d901833510b8af7284fd52e333" "0c2cf3b28f43e091cd7e790a7d3bf761e18195e7c35f5d39b1a880f6bcb33377" "3b5bac2bef0c51a169be7e9b5f80414e662e5eb2e3e3cf126873325e9344d26e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "6fc9e40b4375d9d8d0d9521505849ab4d04220ed470db0b78b700230da0a86c1" "b11edd2e0f97a0a7d5e66a9b82091b44431401ac394478beb44389cf54e6db28" default))
  '(package-selected-packages
-   '(which-key rustic lsp-mode lsp-ui ef-themes mood-line dimmer elcord company flycheck vterm symon)))
+   '(rg neotree all-the-icons which-key rustic lsp-mode lsp-ui ef-themes mood-line dimmer elcord company flycheck vterm symon)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,6 +24,16 @@
   :ensure t)
 
 (use-package vterm
+  :ensure t)
+
+(use-package multi-vterm
+  :config
+  (keymap-global-unset "C-t")
+  (keymap-global-set "C-t t" 'multi-vterm-dedicated-toggle)
+  (keymap-global-set "C-t a" 'multi-vterm)
+  (keymap-global-set "C-t n" 'multi-vterm-next)
+  (keymap-global-set "C-t p" 'multi-vterm-prev)
+  (keymap-global-set "C-t f" 'multi-vterm-project)
   :ensure t)
 
 (use-package free-keys
@@ -61,10 +71,8 @@
   :if (display-graphic-p)
   :ensure t)
 
-(use-package neotree
-  :config (keymap-global-set "M-T" 'neotree-toggle)
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
-  (neotree-toggle)
+(use-package rg
+  :config (rg-enable-default-bindings)
   :ensure t)
 
 (use-package company
@@ -111,7 +119,7 @@
 (add-hook 'after-make-frame-functions 'set-background-for-terminal)
 (add-hook 'window-setup-hook 'set-background-for-terminal)
 
-
+(setq lsp-rust-analyzer-check-all-targets nil)
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
