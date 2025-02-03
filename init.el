212bb786afd5ddfc37ea4efc78f9cd87261a0062
meeeeee :3 <me@visu.do>
2025-02-03 06:28:03 -0800

    update

--- init.el
+++ init.el
@@ -11,13 +11,39 @@
  '(custom-safe-themes
    '("c2c63381042e2e4686166d5d59a09118017b39003e58732b31737deeed454f1c" "a49795828869471ffeeb3753c61e265a75b33fed7c7b7614ee17663f1ef4b3b0" "658f6e811d9972cf53b52b00fa050ef8b30290d901833510b8af7284fd52e333" "0c2cf3b28f43e091cd7e790a7d3bf761e18195e7c35f5d39b1a880f6bcb33377" "3b5bac2bef0c51a169be7e9b5f80414e662e5eb2e3e3cf126873325e9344d26e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "6fc9e40b4375d9d8d0d9521505849ab4d04220ed470db0b78b700230da0a86c1" "b11edd2e0f97a0a7d5e66a9b82091b44431401ac394478beb44389cf54e6db28" default))
  '(package-selected-packages
-   '(rg neotree all-the-icons which-key rustic lsp-mode lsp-ui ef-themes mood-line dimmer elcord company flycheck vterm symon)))
+   '(typst-ts-mode rg neotree all-the-icons which-key rustic lsp-mode lsp-ui ef-themes mood-line dimmer elcord company flycheck vterm symon))
+ '(wakatime-api-key (with-temp-buffer (insert-file-contents ".wakatime_api_key") (buffer-string)))
+ '(wakatime-cli-path "wakatime"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(setq straight-use-package-by-default t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 (use-package elcord
   :config (elcord-mode)
@@ -51,6 +77,9 @@
   (mood-line-mode)
   :ensure t)
 
+(use-package disable-mouse
+  :ensure t)
+(global-disable-mouse-mode)
 ;(use-package monokai-theme
 ;  :config (load-theme 'monokai t)
 ;  :ensure t)
@@ -87,28 +116,44 @@
   :init (which-key-mode)
   :ensure t)
 
-
-(use-package lsp-ui
-  :config (setq lsp-ui-sideline-enable t)
-  (setq lsp-ui-sideline-show-hover t)
-  (setq lsp-ui-sideline-show-code-actions t)
-  (setq lsp-ui-sideline-show-diagnostics t)
-  (setq lsp-ui-doc-enable t)
-  (setq lsp-ui-doc-position 'at-point)
-  (keymap-global-set "M-B" 'lsp-ui-doc-focus-frame)
-  (keymap-global-set "M-A" 'lsp-ui-doc-toggle)
+(use-package eglot
+  :config
+  (add-hook 'rustic-mode-hook 'eglot-ensure)
+  (add-hook 'rust-mode-hook 'eglot-ensure)
+  (keymap-global-set "C-c C-a" 'eglot-code-actions)
   :ensure t)
 
-(use-package lsp-mode
-  :bind-keymap ("C-c e" . lsp-command-map)
-  :config (lsp-diagnostics-mode)
+(use-package typst-ts-mode
+  :straight '(:type git :host codeberg :repo "meow_king/typst-ts-mode")
+  :config
+  (setq typst-ts-watch-options "--open")
   :ensure t)
+
+(use-package websocket)
+(use-package typst-preview
+  :straight '(:type git :host github :repo "havarddj/typst-preview.el")
+  :config
+  (setq typst-preview-browser "chromium")
+  :ensure t)
+
+(with-eval-after-load 'eglot
+  (with-eval-after-load 'typst-ts-mode
+    (add-to-list 'eglot-server-programs
+                 `((typst-ts-mode) .
+                   ,(eglot-alternatives `(,typst-ts-lsp-download-path
+                                          "tinymist"
+                                          "typst-lsp"))))))
 
 (use-package rustic
   :ensure t)
 
 (use-package org
   
+  :ensure t)
+
+(use-package wakatime-mode
+  :config
+  (global-wakatime-mode)
   :ensure t)
 
 (defun set-background-for-terminal (&optional frame)
