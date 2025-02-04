ac2c78ddfbb8429c04c49cb89b0d9a30ac059bfb
meeeeee :3 <me@visu.do>
2025-02-03 22:52:25 -0800

    whoop

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
    '("c2c63381042e2e4686166d5d59a09118017b39003e58732b31737deeed454f1c" "a49795828869471ffeeb3753c61e265a75b33fed7c7b7614ee17663f1ef4b3b0" "658f6e811d9972cf53b52b00fa050ef8b30290d901833510b8af7284fd52e333" "0c2cf3b28f43e091cd7e790a7d3bf761e18195e7c35f5d39b1a880f6bcb33377" "3b5bac2bef0c51a169be7e9b5f80414e662e5eb2e3e3cf126873325e9344d26e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "6fc9e40b4375d9d8d0d9521505849ab4d04220ed470db0b78b700230da0a86c1" "b11edd2e0f97a0a7d5e66a9b82091b44431401ac394478beb44389cf54e6db28" default))
  '(package-selected-packages
    '(typst-ts-mode rg neotree all-the-icons which-key rustic lsp-mode lsp-ui ef-themes mood-line dimmer elcord company flycheck vterm symon))
- '(wakatime-api-key (with-temp-buffer (insert-file-contents ".wakatime_api_key") (buffer-string)))
+ '(wakatime-api-key (with-temp-buffer (insert-file-contents "~/.emacs.d/.wakatime_api_key") (buffer-string)))
  '(wakatime-cli-path "wakatime"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
