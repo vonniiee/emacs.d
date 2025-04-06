9ff4a8be1096f01d2833b4391fba34f6d7782887
siobhan <iam@vonnie.dev>
2025-04-06 06:55:52 -0700

    theme and a few other things

--- init.el
+++ init.el
@@ -9,10 +9,25 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("c2c63381042e2e4686166d5d59a09118017b39003e58732b31737deeed454f1c" "a49795828869471ffeeb3753c61e265a75b33fed7c7b7614ee17663f1ef4b3b0" "658f6e811d9972cf53b52b00fa050ef8b30290d901833510b8af7284fd52e333" "0c2cf3b28f43e091cd7e790a7d3bf761e18195e7c35f5d39b1a880f6bcb33377" "3b5bac2bef0c51a169be7e9b5f80414e662e5eb2e3e3cf126873325e9344d26e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "6fc9e40b4375d9d8d0d9521505849ab4d04220ed470db0b78b700230da0a86c1" "b11edd2e0f97a0a7d5e66a9b82091b44431401ac394478beb44389cf54e6db28" default))
+   '("13096a9a6e75c7330c1bc500f30a8f4407bd618431c94aeab55c9855731a95e1"
+     "c2c63381042e2e4686166d5d59a09118017b39003e58732b31737deeed454f1c"
+     "a49795828869471ffeeb3753c61e265a75b33fed7c7b7614ee17663f1ef4b3b0"
+     "658f6e811d9972cf53b52b00fa050ef8b30290d901833510b8af7284fd52e333"
+     "0c2cf3b28f43e091cd7e790a7d3bf761e18195e7c35f5d39b1a880f6bcb33377"
+     "3b5bac2bef0c51a169be7e9b5f80414e662e5eb2e3e3cf126873325e9344d26e"
+     "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223"
+     "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"
+     "6fc9e40b4375d9d8d0d9521505849ab4d04220ed470db0b78b700230da0a86c1"
+     "b11edd2e0f97a0a7d5e66a9b82091b44431401ac394478beb44389cf54e6db28"
+     default))
  '(package-selected-packages
-   '(typst-ts-mode rg neotree all-the-icons which-key rustic lsp-mode lsp-ui ef-themes mood-line dimmer elcord company flycheck vterm symon))
- '(wakatime-api-key (with-temp-buffer (insert-file-contents "~/.emacs.d/.wakatime_api_key") (buffer-string)))
+   '(typst-ts-mode rg neotree all-the-icons which-key rustic lsp-mode
+		   lsp-ui ef-themes mood-line dimmer elcord company
+		   flycheck vterm symon))
+ '(wakatime-api-key
+   (with-temp-buffer
+     (insert-file-contents "~/.emacs.d/.wakatime_api_key")
+     (buffer-string)))
  '(wakatime-cli-path "wakatime"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -58,6 +73,9 @@
 
 (add-to-list 'default-frame-alist
 	     '(vertical-scroll-bars . nil))
+
+(use-package bluetooth
+  :ensure t)
 
 (use-package exwm)
 
@@ -152,10 +170,18 @@
 ;  :config (load-theme 'kaolin-eclipse t)
 ;  :ensure t)
 
-(use-package ef-themes
-  :config (load-theme 'ef-bio t)
+;(use-package ef-themes
+;  :config (load-theme 'ef-bio t)
+;  :ensure t)
+
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+	doom-themes-enable-italic t)
+  (load-theme 'doom-challenger-deep)
   :ensure t)
 
+  :ensure t
 (use-package all-the-icons
   :if (display-graphic-p)
   :ensure t)
@@ -203,6 +229,10 @@
   (setq typst-preview-browser "chromium")
   :ensure t)
 
+(use-package prism
+  :straight '(:type git :host github :repo "alphapapa/prism.el")
+  :ensure t)
+
 ;(with-eval-after-load 'eglot
 ;  (with-eval-after-load 'typst-ts-mode
 ;    (add-to-list 'eglot-server-programs
@@ -211,16 +241,14 @@
 ;                                          "tinymist"
 ;                                          "typst-lsp"))))))
 
+(use-package magit
+  :ensure t)
+
 (use-package rustic
   :ensure t)
 
 (use-package org
   
-  :ensure t)
-
-(use-package wakatime-mode
-  :config
-  (global-wakatime-mode)
   :ensure t)
 
 (defun set-background-for-terminal (&optional frame)
