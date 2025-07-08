11cf72eca517cf3476cb404c059bb8662c8b4927
siobhan <iam@vonnie.dev>
2025-07-08 12:43:50 -0700

    okay actually fix touchscreen for realsies this time

--- machine/UX8406MA.el
+++ machine/UX8406MA.el
@@ -3,6 +3,8 @@
 	  (lambda ()
 	    (start-process-shell-command
 	     "xrandr" nil "xrandr --output eDP-1 --above eDP-2 --auto")))
-(shell-command "xinput --map-to-output \"ELAN9008:00 04F3:4259\" \"eDP-1\"")
-(shell-command "xinput --map-to-output \"ELAN9009:00 04F3:42EC\" \"eDP-2\"")
+(add-hook 'exwm-init-hook
+	  (lambda ()
+	    (shell-command "xinput --map-to-output \"ELAN9008:00 04F3:4259\" \"eDP-1\"")
+	    (shell-command "xinput --map-to-output \"ELAN9009:00 04F3:42EC\" \"eDP-2\"")))
 
